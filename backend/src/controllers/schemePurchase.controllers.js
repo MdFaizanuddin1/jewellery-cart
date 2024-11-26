babd1e236dca8ad849806e0a8a125bf6db046f96
MdFaizanuddin1 <md18faizan@gmail.com>
2024-11-26 18:19:53 +0530

    change get all subscribers logic

--- backend/src/controllers/schemePurchase.controllers.js
+++ backend/src/controllers/schemePurchase.controllers.js
@@ -49,6 +49,80 @@ const subscribe = asyncHandler(async (req, res) => {
     );
 });
 
+// const getAllSubscribers = asyncHandler(async (req, res) => {
+//   const admin = await User.findById(req.user._id);
+
+//   if (!admin) {
+//     throw new ApiError(404, "admin not found");
+//   }
+
+//   if (admin.role != "admin") {
+//     throw new ApiError(404, "You are not authorized");
+//   }
+
+//   // const schemes await Scheme.find().populate (subscribedBy");
+
+//   const schemes = await Scheme.find();
+
+//   // console.log('scheme is', schemes);
+
+//   const subscribedBy = [];
+//   schemes.map((scheme) =>
+//     scheme.subscribedBy.map((ele) => subscribedBy.push(ele)),
+//   );
+
+//   // console.log("subscribedBy is", subscribedBy)
+//   // " const subscribers = [];
+//   // subscribedBy.map(async (ele) => {
+//   // const user await User.findById(ele).select("-password");
+//   // console.log('user is, user)
+//   // subscribers.push (user)
+//   // });
+//   // console.log('subscribers is', subscribers)
+
+//   let subscribers = [];
+//   subscribers = await Promise.all(
+//     subscribedBy.map(async (ele) => {
+//       const user = await User.findById(ele).select("-password");
+
+//       // console.log("Fetched user:", user); // Log each fetched user
+
+//       return user;
+//     }),
+//   );
+
+//   //console.log("All subscribers:", subscribers)
+
+//   if (!subscribers) {
+//     throw new ApiError(500, "No subscribers was found");
+//   }
+
+//   console.log("subs is", subscribers)
+
+//   const idUserNameFromSubscribers = subscribers.map((ele) => {
+//     return {
+//       id: ele._id,
+
+//       userName: ele.userName,
+
+//       subscribedSchemes: ele.subscribedSchemes,
+//     };
+//   });
+
+//   if (!idUserNameFromSubscribers) {
+//     throw new ApiError(500, "Error while getting username and Id");
+//   }
+//   return res
+//     .status(200)
+//     .json(
+//       new ApiResponse(
+//         200,
+//         idUserNameFromSubscribers,
+//         `total ${subscribers.length} subscribers of all schemes found`,
+//       ),
+//     );
+// });
+
 const getAllSubscribers = asyncHandler(async (req, res) => {
   const admin = await User.findById(req.user._id);
 
@@ -59,68 +133,28 @@ const getAllSubscribers = asyncHandler(async (req, res) => {
   if (admin.role != "admin") {
     throw new ApiError(404, "You are not authorized");
   }
+  const subscribers = await SchemePurchase.find()
+    .populate("purchasedBy", "userName email phone")
+    .populate("scheme", "name thumbnailImg schemeType");
 
-  // const schemes await Scheme.find().populate (subscribedBy");
-
-  const schemes = await Scheme.find();
-
-  // console.log('scheme is', schemes);
-
-  const subscribedBy = [];
-  schemes.map((scheme) =>
-    scheme.subscribedBy.map((ele) => subscribedBy.push(ele)),
-  );
-
-  // console.log("subscribedBy is", subscribedBy)
-  // " const subscribers = [];
-  // subscribedBy.map(async (ele) => {
-  // const user await User.findById(ele).select("-password");
-  // console.log('user is, user)
-  // subscribers.push (user)
-  // });
-  // console.log('subscribers is', subscribers)
-
-  let subscribers = [];
-  subscribers = await Promise.all(
-    subscribedBy.map(async (ele) => {
-      const user = await User.findById(ele).select("-password");
-
-      // console.log("Fetched user:", user); // Log each fetched user
-
-      return user;
-    }),
-  );
-
-  //console.log("All subscribers:", subscribers)
-
-  if (!subscribers) {
-    throw new ApiError(500, "No subscribers was found");
-  }
-
-  // console.log("subs is", subscribers)
-
-  const idUserNameFromSubscribers = subscribers.map((ele) => {
-    return {
-      id: ele._id,
-
-      userName: ele.userName,
-
-      subscribedSchemes: ele.subscribedSchemes,
-    };
-  });
-
-  if (!idUserNameFromSubscribers) {
-    throw new ApiError(500, "Error while getting username and Id");
-  }
   return res
     .status(200)
     .json(
       new ApiResponse(
         200,
-        idUserNameFromSubscribers,
-        `total ${subscribers.length} subscribers of all schemes found`,
+        subscribers.length > 0 ? subscribers : {},
+        subscribers.length > 0
+          ? `total number of subscribers is ${subscribers.length}`
+          : "No subscribers found",
       ),
     );
 });
 
-export { subscribe, getAllSubscribers };
+
+
+export {
+  subscribe,
+  getAllSubscribers,
+  getUserSubscribedSchemes,
+  getSchemeSubscribers,
+};
--- backend/src/routes/schemePurchase.routes.js
+++ backend/src/routes/schemePurchase.routes.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { getAllSubscribers, subscribe } from "../controllers/schemePurchase.controllers.js";
+import {
+  getAllSubscribers,
+  getSchemeSubscribers,
+  getUserSubscribedSchemes,
+  subscribe,
+} from "../controllers/schemePurchase.controllers.js";
 import { verifyJWT } from "../middlewares/userAuth.middlewares.js";
 
 const router = Router();
 
 router.post("/subscribe/:schemeId", verifyJWT, subscribe);
 router.get("/getAllSubscribers", verifyJWT, getAllSubscribers);
+
+// Get all subscribed schemes of a specific user
+router.get("/user/subscribed-schemes", verifyJWT, getUserSubscribedSchemes);
+
+// Get all subscribers of a specific scheme
+router.get("/scheme/:schemeId", verifyJWT, getSchemeSubscribers);
 
 export default router;
